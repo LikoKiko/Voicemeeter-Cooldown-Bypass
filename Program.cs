a1cb6e04fe7a8ae7ef47f3d1bbefd4ed3e6a3cbd
LikoKiko <145937091+LikoKiko@users.noreply.github.com>
2026-03-21 00:56:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Swed64;
+
+namespace VoicemeeterCooldownBypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looking for voicemeeter8.exe...");
+            try
+            {
+                Swed swed = new Swed("voicemeeter8");
+                Console.WriteLine("Successfully attached!");
+                IntPtr moduleBase = swed.GetModuleBase("voicemeeter8.exe");
+                IntPtr targetAddress = moduleBase + 0x13B518;
+                Console.WriteLine("Press Ctrl+C to stop");
+                while (true)
+                {
+                    swed.WriteInt(targetAddress, 1);
+                    Thread.Sleep(100); 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not attach to VoiceMeeter.");
+                Console.WriteLine("Make sure it is running and try running this tool as Administrator");
+                Console.WriteLine($"Error Details: {ex.Message}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
